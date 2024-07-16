57ebd8366af105be4fa5b3ee984fe8c4ae181feb
xiangzhe <xiangzhe@xsky.com>
2024-07-16 18:42:16 +0800

    update
    Signed-off-by: xiangzhe <xiangzhe@xsky.com>

--- contracts/FitMiles.sol
+++ contracts/FitMiles.sol
@@ -2,21 +2,32 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract FitMiles is ERC20 {
-    address public owner;
+contract FitMiles is ERC20, Ownable {
     mapping(address => uint256) public milesRecord;
 
     event MilesRecorded(address indexed user, uint256 miles);
+    event TokensClaimed(address indexed user, uint256 amount);
 
-    constructor() ERC20("FitMiles Token", "FIT") {
-        owner = msg.sender;
-    }
+    constructor() ERC20("FitMiles Token", "FIT") {}
 
     function recordMiles(uint256 _miles) external {
         require(_miles > 0, "Miles must be greater than zero");
-        _mint(msg.sender, _miles);
         milesRecord[msg.sender] += _miles;
         emit MilesRecorded(msg.sender, _miles);
+    }
+
+    function claimTokens() external {
+        uint256 miles = milesRecord[msg.sender];
+        require(miles > 0, "No miles to claim");
+
+        milesRecord[msg.sender] = 0;
+        _mint(msg.sender, miles);
+        emit TokensClaimed(msg.sender, miles);
+    }
+
+    function getClaimableMiles(address _user) external view returns (uint256) {
+        return milesRecord[_user];
     }
 }
